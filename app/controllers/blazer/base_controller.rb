86ad8428959f1ef555d95b52bb8c1847b49f160e
Rob Cameron <cannikinn@gmail.com>
2017-03-23 16:04:13 -0700

    respond_to(‘current_user’) always returns false, even though just calling `current_user` works fine

--- app/controllers/blazer/base_controller.rb
+++ app/controllers/blazer/base_controller.rb
@@ -92,7 +92,7 @@ module Blazer
       helper_method :variable_params
 
       def blazer_user
-        send(Blazer.user_method) if Blazer.user_method && respond_to?(Blazer.user_method)
+        send(Blazer.user_method) if Blazer.user_method #&& respond_to?(Blazer.user_method)
       end
       helper_method :blazer_user
 
